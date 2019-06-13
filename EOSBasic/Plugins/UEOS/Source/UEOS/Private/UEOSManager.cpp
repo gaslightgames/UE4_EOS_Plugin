93979ac91bc14d72ba439279f1b0ae17d97e7a90
RyroNZ <ryan@somethinglogical.co.nz>
2019-06-13 19:06:04 +1200

    Fixed issue where setting the SandboxId was overwriting the ProductId in memory

--- EOSBasic/Plugins/UEOS/Source/UEOS/Private/UEOSManager.cpp
+++ EOSBasic/Plugins/UEOS/Source/UEOS/Private/UEOSManager.cpp
@@ -7,6 +7,7 @@
 #include "Metrics/Metrics.h"
 
 #include "UEOSModule.h"
+#include <string>
 
 // Static Initialization
 UEOSManager* UEOSManager::EOSManager = nullptr;
@@ -85,51 +86,47 @@ bool UEOSManager::InitEOS()
 	EOS_Platform_Options PlatformOptions;
 	PlatformOptions.ApiVersion = EOS_PLATFORM_OPTIONS_API_LATEST;
 
-	FString ProductId;
-	FString SandboxId;
-
 	if( EOSConfig != nullptr )
 	{
 		PlatformOptions.bIsServer = ( EOSConfig->bIsServer ) ? EOS_TRUE : EOS_FALSE;
 
-		ProductId = EOSConfig->ProductId;
-		SandboxId = EOSConfig->SandboxId;
+		std::string ProductId = TCHAR_TO_UTF8(*EOSConfig->ProductId);
+		std::string SandboxId = TCHAR_TO_UTF8(*EOSConfig->SandboxId);
 
-		PlatformOptions.ProductId = TCHAR_TO_UTF8( *ProductId );
-		PlatformOptions.SandboxId = TCHAR_TO_UTF8( *SandboxId );
+		PlatformOptions.ProductId = ProductId.c_str();
+		PlatformOptions.SandboxId = SandboxId.c_str();
+		
+		std::string ClientId = TCHAR_TO_UTF8( *EOSConfig->ClientId );
+		std::string ClientSecret = TCHAR_TO_UTF8( *EOSConfig->ClientSecret );
 
-		PlatformOptions.ClientCredentials.ClientId = ( EOSConfig->ClientId.IsEmpty() ) ? nullptr : TCHAR_TO_UTF8( *EOSConfig->ClientId );
-		PlatformOptions.ClientCredentials.ClientSecret = ( EOSConfig->ClientSecret.IsEmpty() ) ? nullptr : TCHAR_TO_UTF8( *EOSConfig->ClientSecret );
+		PlatformOptions.ClientCredentials.ClientId = ( EOSConfig->ClientId.IsEmpty() ) ? nullptr : ClientId.c_str();
+		PlatformOptions.ClientCredentials.ClientSecret = ( EOSConfig->ClientSecret.IsEmpty() ) ? nullptr : ClientSecret.c_str();
 
 		if( EOSConfig->bAllowReservedPlatforms )
 		{
 			// Taken from the Sample, but this function and it's include could not be found.
 			// Left for posterity, but defaulting to NULL.
 			//SetReservedPlatformOptions( PlatformOptions );
-			PlatformOptions.Reserved = NULL;
+			PlatformOptions.Reserved = nullptr;
 		}
 		else
 		{
-			PlatformOptions.Reserved = NULL;
+			PlatformOptions.Reserved = nullptr;
 		}
-	}
-	else
-	{
-		PlatformOptions.bIsServer = EOS_FALSE;
-		PlatformOptions.ClientCredentials.ClientId = nullptr;
-		PlatformOptions.ClientCredentials.ClientSecret = nullptr;
-		PlatformOptions.Reserved = NULL;
+		
+		PlatformHandle = EOS_Platform_Create( &PlatformOptions );
+
+		
 	}
 
-	PlatformHandle = EOS_Platform_Create( &PlatformOptions );
-
-	if( PlatformHandle == nullptr )
+	if (PlatformHandle == nullptr)
 	{
-		MessageText = FString::Printf( TEXT( "[EOS SDK | Plugin] Platform Create - Failed!" ) );
-		UE_LOG( UEOSLog, Warning, TEXT( "%s" ), *MessageText );
+		MessageText = FString::Printf(TEXT("[EOS SDK | Plugin] Platform Create - Failed!"));
+		UE_LOG(UEOSLog, Warning, TEXT("%s"), *MessageText);
 
 		return false;
 	}
+	
 
 	MessageText = FString::Printf( TEXT( "[EOS SDK | Plugin] Platform Create - Success!" ) );
 	UE_LOG( UEOSLog, Warning, TEXT( "%s" ), *MessageText );
